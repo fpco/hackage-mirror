51b7ea38c89783beae6d90d1a3586599dea1a4c9
Tim Dysinger <tim@dysinger.net>
2015-03-17 14:35:30 -1000

    consolidate main & options

--- main/Main.hs
+++ main/Main.hs
@@ -1,8 +1,7 @@
 import Control.Applicative ( Applicative((<*>)), (<$>) )
 import Hackage.Mirror ( Options(..), mirrorHackage )
 import Options.Applicative
-    ( Parser,
-      helper,
+    ( helper,
       execParser,
       value,
       switch,
@@ -15,20 +14,27 @@ import Options.Applicative
       fullDesc,
       (<>) )
 
-options :: Parser Options
-options = Options
-    <$> switch (long "verbose" <> help "Display verbose output")
-    <*> switch (long "rebuild" <> help "Don't mirror; used for rebuilding")
-    <*> strOption (long "from" <> value "http://hackage.haskell.org"
-                   <> help "Base URL to mirror from")
-    <*> strOption (long "to" <> help "Base URL of server mirror to")
-    <*> strOption (long "access" <> value "" <> help "S3 access key")
-    <*> strOption (long "secret" <> value "" <> help "S3 secret key")
-
 main :: IO ()
-main = execParser opts >>= mirrorHackage
-  where
-    opts = info (helper <*> options)
-                (fullDesc
-                 <> progDesc "Mirror the necessary parts of Hackage"
-                 <> header "hackage-mirror - mirror only the minimum")
+main = execParser optsParser >>= mirrorHackage
+  where optsParser =
+          info (helper <*> options)
+               (fullDesc <>
+                progDesc "Mirror the necessary parts of Hackage" <>
+                header "hackage-mirror - mirror only the minimum")
+        options =
+          Options <$>
+          switch (long "verbose" <>
+                  help "Display verbose output") <*>
+          switch (long "rebuild" <>
+                  help "Don't mirror; used for rebuilding") <*>
+          strOption (long "from" <>
+                     value "http://hackage.haskell.org" <>
+                     help "Base URL to mirror from") <*>
+          strOption (long "to" <>
+                     help "Base URL of server mirror to") <*>
+          strOption (long "access" <>
+                     value "" <>
+                     help "S3 access key") <*>
+          strOption (long "secret" <>
+                     value "" <>
+                     help "S3 secret key")
